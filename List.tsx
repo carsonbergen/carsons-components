80417250a08efac6ea4bab1d1cb34c8998da09ac
Carson Bergen <bergencarson@gmail.com>
2023-12-15 12:15:16 -0700

    Added LinkList

---
+++ List.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import { randInt } from "three/src/math/MathUtils";
+import { LinkType } from "./types";
+
+// List of links
+export function LinkList(props: { items: LinkType[] }) {
+    const hash = (str: string): number => {
+        let code = 0;
+        if (str.length === 0) {
+            return code;
+        }
+        for (let i = 0; i < str.length; i++) {
+            const char = str.charCodeAt(i);
+            code = code + char + randInt(0, str.length);
+            // Convert to 32-bit integer
+            code |= 0;
+        }
+        return Math.abs(code);
+    }
+    
+    return (
+        <>
+            <ul className="flex flex-row font-eb-garamond">
+                {
+                    props.items.map((link: LinkType) => (
+                        <li
+                            id={link.name}
+                            key={hash(link.link)}
+                        >
+                            <Link
+                                href={link.link}
+                                // target="_blank"
+                                // rel="noopener noreferrer"
+                                className="text-primary pr-2"
+                            >
+                                {link.name}
+                            </Link>
+                        </li>
+                    ))
+                }
+            </ul>
+        </>
+    );
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Requirements
+- Tailwindcss
+- React
+- NextJS
